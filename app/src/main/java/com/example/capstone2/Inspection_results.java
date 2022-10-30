1cbefc62829971a1c980eeedae832899e3a46167
hcm01316 <hcm01316@gmail.com>
2022-10-31 00:28:21 +0900

    beta csv parser ing~

--- app/src/main/java/com/example/capstone2/Inspection_results.java
+++ app/src/main/java/com/example/capstone2/Inspection_results.java
@@ -9,10 +9,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
@@ -22,7 +22,6 @@ public class Inspection_results extends AppCompatActivity {
     TextView link;
     TextView result;
     String text;
-    String[] url = {};  //링크 마지막에 /지워야 함
     private Object String;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +36,30 @@ public class Inspection_results extends AppCompatActivity {
         QRscan_stop = (Button) findViewById(R.id.close);
         Join_link = (Button) findViewById(R.id.join_site);
         link.setText("링크는" + text + "입니다.");
-//        try {
-//
-//            InputStreamReader is = new InputStreamReader(getResources().openRawResource(R.raw.badsite));
-//            BufferedReader reader = new BufferedReader(is);
-//            CSVReader read = new CSVReader(reader);
-//            String record;
-//            int link_count = 0;
-//            while ((record = read.readNext()) != null) {
-//                url[link_count] = record;
-//                if (url[link_count].equals(text))                            // qrcode랑 링크랑 같다면
-//                {
-//                    result.setText("해당 링크는 취약한 사이트 입니다.");
-//                    break;
-//                } else {
-//                    link_count++;
-//                }
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        result.setText("해당 링크는 안전합니다.");
+        try {
+
+            InputStreamReader is = new InputStreamReader(getResources().openRawResource(R.raw.badsite));
+            BufferedReader reader = new BufferedReader(is);
+            CSVReader read = new CSVReader(reader);
+            String[] record = null;
+            int link_count = 0;
+            while ((record = read.readNext()) != null) {
+                if (record[link_count].equals(text))                            // qrcode랑 링크랑 같다면
+                {
+                    result.setText("해당 링크는 취약한 사이트 입니다.");
+                    break;
+                } else {
+                    link_count++;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        result.setText("해당 링크는 안전합니다.");
 
         QRscan_stop.setOnClickListener(new View.OnClickListener() {
 
