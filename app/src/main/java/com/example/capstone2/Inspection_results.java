670f8a25ee30d7f83d6ed46a9a8d3daa6991c7bb
hcm01316 <hcm01316@gmail.com>
2022-10-30 19:33:33 +0900

    beta csv parser

--- app/src/main/java/com/example/capstone2/Inspection_results.java
+++ app/src/main/java/com/example/capstone2/Inspection_results.java
@@ -8,91 +8,70 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-
-import org.w3c.dom.Text;
+import com.opencsv.CSVReader;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-public class Inspection_results extends AppCompatActivity
-{
+
+public class Inspection_results extends AppCompatActivity {
     Button QRscan_stop;
     Button Join_link;
     TextView link;
     TextView result;
-    String[] url={"https://attsbcbell-104995.weeblysite.com","https://guacharen.tk/web"};  //링크 마지막에 /지워야 함
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    String text;
+    String[] url = {};  //링크 마지막에 /지워야 함
+    private Object String;
+
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspection_result);
         Intent intent = getIntent();
-        String text=intent.getStringExtra("link");
+        text = intent.getStringExtra("link");
 
-        result=(TextView) findViewById(R.id.scan_result);
-        link = (TextView) findViewById(R.id.qr_link) ;
-        QRscan_stop= (Button)findViewById(R.id.close);
-        Join_link= (Button)findViewById(R.id.join_site);
-        link.setText("링크는"+text+"입니다.");
-        for(int i=0;i<2;i++)
-        {
-            if(url[i].equals(text))
-            {
-                result.setText("해당 링크는 취약한 사이트 입니다.");
-                break;
-            }
-            else
-            {
-                result.setText("해당 링크는 안전합니다.");
-            }
-        }
-        QRscan_stop.setOnClickListener(new View.OnClickListener()
-        {
+        result = (TextView) findViewById(R.id.scan_result);
+        link = (TextView) findViewById(R.id.qr_link);
+        QRscan_stop = (Button) findViewById(R.id.close);
+        Join_link = (Button) findViewById(R.id.join_site);
+        link.setText("링크는" + text + "입니다.");
+//        try {
+//
+//            InputStreamReader is = new InputStreamReader(getResources().openRawResource(R.raw.badsite));
+//            BufferedReader reader = new BufferedReader(is);
+//            CSVReader read = new CSVReader(reader);
+//            String record;
+//            int link_count = 0;
+//            while ((record = read.readNext()) != null) {
+//                url[link_count] = record;
+//                if (url[link_count].equals(text))                            // qrcode랑 링크랑 같다면
+//                {
+//                    result.setText("해당 링크는 취약한 사이트 입니다.");
+//                    break;
+//                } else {
+//                    link_count++;
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        result.setText("해당 링크는 안전합니다.");
+
+        QRscan_stop.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
 
             }
         });
-        Join_link.setOnClickListener(new View.OnClickListener()
-        {
+        Join_link.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(text));                      //qr주소를 인터넷으로 연결
                 startActivity(intent);
             }
         });
-    }
-    public String readFile(String fileName) throws IOException
-    {
-        String filecontents="";
-        try{
-            InputStream iStream = openFileInput(fileName);
-            if(iStream !=null)
-            {
-                InputStreamReader iStreamReader=new InputStreamReader(iStream);
-                BufferedReader bufferedReader=new BufferedReader(iStreamReader);
-                String temp="";
-                StringBuffer sBuffer=new StringBuffer();
-                while((temp= bufferedReader.readLine())!=null)
-                {
-                    sBuffer.append(temp);
-                }
-                iStream.close();
-                filecontents=sBuffer.toString();
-
-            }
-        } catch(FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        return filecontents;
     }
 }
